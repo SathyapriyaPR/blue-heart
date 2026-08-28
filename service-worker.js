e074ab3a3b0d73327f9543cb141ddc26fed69137
Sathyapriya P R <sathyapriyaprar246@gmail.com>
2026-08-28 13:14:05 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,84 @@
-const CACHE_NAME = "blue-heart-v6-test";
+const CACHE_NAME =
+    "blue-heart-v6-debug";
 
-const FILES_TO_CACHE = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./timetable.js",
-    "./manifest.json",
-    "./icon/icon-192.png",
-    "./icon/icon-512.png"
-];
 
-self.addEventListener("install", event => {
+/* =========================================================
+   INSTALL
+========================================================= */
 
-    console.log("Blue Heart SW installing");
+self.addEventListener(
+    "install",
+    event => {
 
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
-    );
+        console.log(
+            "Blue Heart service worker installing"
+        );
 
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-
-    console.log("Blue Heart SW activated");
-
-    event.waitUntil(
-        caches
-            .keys()
-            .then(keys =>
-                Promise.all(
-                    keys
-                        .filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
-                )
-            )
-    );
-
-    self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-
-    if (event.request.method !== "GET") {
-        return;
+        self.skipWaiting();
     }
+);
 
-    event.respondWith(
-        caches.match(event.request)
-            .then(cached => cached || fetch(event.request))
-    );
-});
 
-self.addEventListener("notificationclick", event => {
+/* =========================================================
+   ACTIVATE
+========================================================= */
 
-    event.notification.close();
+self.addEventListener(
+    "activate",
+    event => {
 
-    event.waitUntil(
-        clients.openWindow("./")
-    );
-});
+        console.log(
+            "Blue Heart service worker activating"
+        );
+
+        event.waitUntil(
+            self.clients.claim()
+        );
+    }
+);
+
+
+/* =========================================================
+   FETCH
+========================================================= */
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+            return;
+        }
+
+        /*
+           Network only for this diagnostic test.
+           No cache installation can fail.
+        */
+
+        event.respondWith(
+            fetch(
+                event.request
+            )
+        );
+    }
+);
+
+
+/* =========================================================
+   NOTIFICATION CLICK
+========================================================= */
+
+self.addEventListener(
+    "notificationclick",
+    event => {
+
+        event.notification.close();
+
+        event.waitUntil(
+            clients.openWindow("./")
+        );
+    }
+);
